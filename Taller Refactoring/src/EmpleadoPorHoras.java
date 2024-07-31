bfdc166db92802e8e11f396cfda3616567699896
Angie Alfonso <aalfonso@espol.edu.ec>
2024-07-31 17:56:33 -0500

    Correción

--- Taller Refactoring/src/EmpleadoPorHoras.java
+++ Taller Refactoring/src/EmpleadoPorHoras.java
@@ -1,13 +1,25 @@
 public class EmpleadoPorHoras extends Empleado {
+    private double tarifaHora;
+
 
     public EmpleadoPorHoras(String nombre, int horasTrabajadas, double tarifaHora, String genero) {
-        super(nombre, 0, horasTrabajadas, tarifaHora, genero);
+        super(nombre, 0, horasTrabajadas, genero);
+        this.tarifaHora = tarifaHora;
         super.setSalarioBase(calcularSalario());
     }
 
     @Override
     public double calcularSalario() {
-        return super.getHorasTrabajadas() * super.getTarifaHora();
+        return super.getHorasTrabajadas() * getTarifaHora();
+    }
+
+    
+    public double getTarifaHora() {
+        return tarifaHora;
+    }
+
+    public void setTarifaHora(double tarifaHora) {
+        this.tarifaHora = tarifaHora;
     }
 
     // Más metodos
