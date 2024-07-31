d58ef0d53cbc29d1ae7048005eb8d5b058fba56d
Angie Alfonso <aalfonso@espol.edu.ec>
2024-07-31 17:48:42 -0500

    Inline Temp
    solucionado

--- Taller Refactoring/src/EmpleadoPorHoras.java
+++ Taller Refactoring/src/EmpleadoPorHoras.java
@@ -7,8 +7,7 @@ public class EmpleadoPorHoras extends Empleado {
 
     @Override
     public double calcularSalario() {
-        double salario = super.getHorasTrabajadas() * super.getTarifaHora();
-        return salario;
+        return super.getHorasTrabajadas() * super.getTarifaHora();
     }
 
     // Más metodos
